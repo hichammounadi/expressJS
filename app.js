a5fa9e17ac654279ee836f53728d158b84dc5b58
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-07 14:17:08 +0100

    method post

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ const express = require('express');
 const app = express();
 let {people} = require('./data')
 
+// static assets
+app.use(express.static('./methods-public'))
 
+
+// parse form data in place of bodyParser
+app.use(express.urlencoded({extended: false}))
+app.post('/login',(req,res)=>{
+    // console.log(req.body);
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please Provide Credential')
+})
+
+
+// GET method
 app.get('/api/people', (req,res)=>{
     res.status(200).json({success: true, data: people})
 })
