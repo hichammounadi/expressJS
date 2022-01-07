de6f8f3baa761ca43cfb35e4e67ded6aa8213dfa
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-07 10:13:52 +0100

    query string

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const { products } = require('./data');
@@ -27,6 +28,32 @@ app.get('/api/products/:productID', (req, res) => {
   }
   res.json(singleProduct);
 });
+
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if(sortedProducts < 1) {
+      res.status(200).json({success: true, data: []})
+  }
+  res.status(200).json(sortedProducts);
+//   res.send('hello world');
+});
+
+
+
+
+
+
 
 app.listen(5000, () => {
   console.log('server listening on port 5000...');
