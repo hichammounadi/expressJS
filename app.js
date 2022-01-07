ae94a219091bf1dd71f6b9e9d66eb2522c813d84
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-07 09:12:00 +0100

    express app example

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const createApplication = require('express/lib/express')
+const path = require('path')
+const app = express()
+
+// setup static and middleware
+app.use(express.static('./public'))
+
+app.get('/',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.all('*', (req, res)=>{
+    res.status(404).send('response not found')
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(5000, ()=>{
+    console.log('Server is listening on port 5000...');
+})
