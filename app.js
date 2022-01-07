ea2164d9b1fb5c94f7239ddcd62d9267ca99287d
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-07 15:11:36 +0100

    DELETE method

--- app.js
+++ app.js
@@ -59,6 +59,21 @@ app.put('/api/people/:id', (req,res)=>{
     res.status(200).json({success:true,data:newPeople})
 })
 
+
+
+//Delete method
+
+app.delete('/api/people/:id', (req,res)=>{
+    const person = people.find((person)=> person.id === Number(req.params.id))
+    if(!person){
+        return res
+        .status(400)
+        .json({success:false,msg:`no person with id ${req.params.id}`})
+    }
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+    return res.status(200).json({success:true, data: newPeople})
+})
+
 //HERE THE SERVER
 app.listen(5000, () => {
   console.log('server listening on port 5000...');
