0e5b762e81158e40f9f348ad652f7bcb8d0919b6
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-07 11:39:43 +0100

    adding newFile about the middleware

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
-const logger = require('./logger')
-const authorize = require('./authorize')
-//req >====> middleware >=====> res
 
-app.use([logger, authorize])
-
-app.get('/',  (req, res) => {
-  res.send('Home');
-});
-
-app.get('/about',  (req, res) => {
-  res.send('about');
-});
-
-app.get('/api/products',  (req, res)=> {
-    res.send('Products')
-})
 
 //HERE THE SERVER
 app.listen(5000, () => {
